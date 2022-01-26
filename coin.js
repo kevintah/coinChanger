5804540b31979e1df263ea3af0ce6c7d466c6602
Ibrahim Diallo <ibuvdiallo@gmail.com>
2022-01-25 18:43:49 -0800

    formatting matters :)

--- coin.js
+++ coin.js
@@ -1,150 +1,131 @@
-//Kevin Tah Njokom
-//12-8-2021
-//6:20 PM
-
+/**
+ * @author Kevin Tah Njokom
+ * @copyright public
+ * @description
+ *  - Give me some change!
+**/
 
 var coinsValues = [
-
-				{
-					coin :"penny",
-					value: 0.01},
-				{
-					coin : "nickel",
-					value: 0.05},
-
-				{	coin :"dime",
-					value: 0.1},
-
-				{	coin : "quarter",
-					value : 0.25},
-
-				{	coin : "dollar",
-					value : 1.0}
-
-
-
-				];
+    {
+        coin: "penny",
+        value: 0.01
+    },
+    {
+        coin: "nickel",
+        value: 0.05
+    },
+    {
+        coin: "dime",
+        value: 0.1
+    },
+    {
+        coin: "quarter",
+        value: 0.25
+    },
+    {
+        coin: "dollar",
+        value: 1.0
+    }
+];
 
 var coinsChange = [
+    {
+        coin: "penny",
+        numCoins: 0
+    },
+    {
+        coin: "nickel",
+        numCoins: 0
+    },
+    {
+        coin: "dime",
+        numCoins: 0
+    },
+    {
+        coin: "quarter",
+        numCoins: 0
+    },
+    {
+        coin: "dollar",
+        numCoins: 0
+    }
+];
 
-				{
-					coin :"penny",
-					numCoins: 0 },
-				{
-					coin : "nickel",
-					numCoins: 0},
 
-				{	coin :"dime",
-					numCoins: 0},
-
-				{	coin : "quarter",
-					numCoins : 0},
-
-				{	coin : "dollar",
-					numCoins : 0}
-
-
-
-				];
-
-
-var allValues = [];	
+var allValues = [];
 var maxValue = 0;
-var numberOf = 0;	
+var numberOf = 0;
 var multiplicativeFactor;
 var sub;
 var newAmountNow;
-var amount;	
-var count = 0;	
+var amount;
+var count = 0;
 
+function findMax(allValues) {
+    theMaxValue = Math.max(...allValues);
+    maxValue = theMaxValue;
+}
 
+function drop(allValues) {
+    if (maxValue > amount) {
+        allValues.splice(index, 1);
+        findMax(allValues);
+    }
+}
 
-function findMax(allValues){							    		
-	theMaxValue = Math.max(... allValues );
-	maxValue = theMaxValue;
+function divideIt(amount, maxValue) {
+    numberOf = Math.floor(amount / maxValue);
 }
 
 
-function drop(allValues){											
-
-	if( maxValue > amount){
-
-		allValues.splice(index, 1);
-		findMax(allValues);
-	}
+function updateIndex(maxValue) {
+    index = allValues.indexOf(maxValue);
 }
 
+function updateChange(index, coinsChange) {
 
-
-
-function divideIt(amount, maxValue){		                 					  
-
-	numberOf = Math.floor( amount / maxValue );
+    coinsChange[index].numCoins = numberOf;
 
 }
 
-
-function updateIndex(maxValue){                          	 		
-	index = allValues.indexOf(maxValue);
-}
-
-
-function  updateChange(index,coinsChange){					 		 
-
-	coinsChange[index].numCoins = numberOf;
-
-}
-
-
-
-
-function newAmount(maxValue,amount,allValues){					    
-    
+function newAmount(maxValue, amount, allValues) {
     drop(allValues);
     multiplicativeFactor = coinsValues[index].value;
-    sub = ( multiplicativeFactor * numberOf );
-    newAmountNow = ( amount - sub );
+    sub = (multiplicativeFactor * numberOf);
+    newAmountNow = (amount - sub);
     amount = newAmountNow;
     allValues.splice(index, 1);
-
 }
 
+function loadCoins(coinsValues) {
+    for (var i = 0; i < coinsValues.length; i++) {
 
-
-
-function loadCoins(coinsValues){                            		 
-
-	for(var i = 0; i < coinsValues.length; i++){
-
-		allValues[i] = coinsValues[i].value;
-			}
-        count++;
-	}
-
-
-function change(amount){									 		  
-	
-    if(count == 0){
-	loadCoins(coinsValues);
+        allValues[i] = coinsValues[i].value;
     }
-    
-	findMax(allValues);
-	divideIt(amount, maxValue);
-	updateIndex(maxValue);
-	updateChange(index,coinsChange);
-	newAmount(maxValue,amount,allValues);
+    count++;
+}
 
-	
-    if(newAmount != 0  && allValues.length> 0){
-	updateChange(index,coinsChange)
-	amount = newAmountNow.toFixed(2);
-	change(amount);
- }
+function change(amount) {
+    if (count == 0) {
+        loadCoins(coinsValues);
+    }
 
- console.log(coinsChange);
- console.log(amount);
-	
-}	
+    findMax(allValues);
+    divideIt(amount, maxValue);
+    updateIndex(maxValue);
+    updateChange(index, coinsChange);
+    newAmount(maxValue, amount, allValues);
 
+
+    if (newAmount != 0 && allValues.length > 0) {
+        updateChange(index, coinsChange)
+        amount = newAmountNow.toFixed(2);
+        change(amount);
+    }
+
+    console.log(coinsChange);
+    console.log(amount);
+
+}
 
 change(23.45);
